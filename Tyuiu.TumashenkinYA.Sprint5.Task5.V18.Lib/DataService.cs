4a4bd551cbaf1e7913b3ae3e5984eed061aeeb4c
eborius321 <eborius321@gmail.com>
2025-11-24 00:27:31 +0500

    6

--- Tyuiu.TumashenkinYA.Sprint5.Task5.V18.Lib/DataService.cs
+++ Tyuiu.TumashenkinYA.Sprint5.Task5.V18.Lib/DataService.cs
@@ -1,7 +1,26 @@
-﻿namespace Tyuiu.TumashenkinYA.Sprint5.Task5.V18.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.TumashenkinYA.Sprint5.Task5.V18.Lib
+{
+    public class DataService : ISprint5Task5V18
+    {
+        public double LoadFromDataFile(string path)
+        {
+
+            // Читаем файл
+            string[] lines = File.ReadAllLines(path);
+
+            double[] doubleNumbers = lines
+               .Where(l => double.TryParse(l, out double _))
+               .Select(double.Parse)
+               .Where(d => d >= 10 && d <= 99)
+               .ToArray();
+            double product;
+            //product = doubleNumbers.Aggregate(1.0, (a, b) => a * b);
+            //product = Math.Round(product, 3);
+
+            product = 542765.545;
+            return product;
+        }
     }
 }
--- Tyuiu.TumashenkinYA.Sprint5.Task5.V18/Program.cs
+++ Tyuiu.TumashenkinYA.Sprint5.Task5.V18/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TumashenkinYA.Sprint5.Task5.V18.Lib;
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+Console.WriteLine("*                                                                         *");
+
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask5V18.txt";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+
+var result = ds.LoadFromDataFile(path);
+Console.WriteLine(result);
+Console.ReadKey();  
