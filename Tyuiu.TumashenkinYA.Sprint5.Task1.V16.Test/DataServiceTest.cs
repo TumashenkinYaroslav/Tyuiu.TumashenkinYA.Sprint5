555a4fbaf795ceb68f4f66390dca6ecb32701d71
eborius321 <eborius321@gmail.com>
2025-11-23 23:59:45 +0500

    1

---
+++ Tyuiu.TumashenkinYA.Sprint5.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TumashenkinYA.Sprint5.Task1.V16.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint5.Task1.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Convert.ToString(Path.GetTempPath()), "OutPutFileTask1.txt");
+
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(true, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint5.Task1.V16/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.TumashenkinYA.Sprint5.Task1.V16.Lib;
+DataService ds = new DataService();
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано функция, произвести табулирование на заданном диапазоне [-5,5] с   *");
+Console.WriteLine("* шагом 1. Произвести проверку на ноль. При делении на ноль вернуть ноль  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int start = -5;
+int end = 5;
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(start, end);
+Console.WriteLine("Файл: " + res);
+
+Console.WriteLine("Создан!");
+
+
+
+Console.ReadKey();
